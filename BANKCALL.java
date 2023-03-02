5f0d5e3813981ff08736777d58892dac4d7e71e5
Rajdeep Datta <rajdeepdatta2016@gmail.com>
2023-03-03 01:47:22 +0530

    Added Bank Interface Files

---
+++ BANKCALL.java
@@ -0,0 +1,9 @@
+public class BANKCALL {
+    public static void main(String args[])
+    {
+        BANK_INTERFACE o = new BANK_INTERFACE();
+        o.getData();
+        o.showData();
+    }
+    
+}
