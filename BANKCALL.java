eb1b8de69b721b4aa5803c64fab67b691750550a
Rajdeep Datta <rajdeepdatta2016@gmail.com>
2023-03-24 16:50:28 +0530

    deleted

--- BANKCALL.java
+++
@@ -1,9 +0,0 @@
-public class BANKCALL {
-    public static void main(String args[])
-    {
-        BANK_INTERFACE o = new BANK_INTERFACE();
-        o.getData();
-        o.showData();
-    }
-    
-}
